e86e58dc97f99082fb5e162ea8a68d1341d13817
zy <1840354092@qq.com>
2022-06-06 17:32:51 +0800

    🍁

--- src/pages/app/app.js
+++ src/pages/app/app.js
@@ -2,18 +2,23 @@ import React from 'react';
 import {Outlet} from "react-router-dom";
 import Header from "./moudel/header";
 import {Divider} from "antd";
+import './app.scss'
 
 const App = () => {
     return (
-        <div className="app">
-            <section>
-                <Header/>
+        <section className="app">
+            <section className="headerColumn">
+                11
             </section>
-            <Divider/>
-            <section>
-                <Outlet/>
+            <section className="main">
+                <section className="primaryColumn">
+                    <Outlet/>
+                </section>
+                <section className="sidebarColumn">
+                    123
+                </section>
             </section>
-        </div>
+        </section>
     );
 }
 
