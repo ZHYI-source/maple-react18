bca6cf1532b008b2f9b278bedad42258145f7ba9
zy <1840354092@qq.com>
2022-06-06 17:02:05 +0800

    🍁

--- src/pages/app/app.js
+++ src/pages/app/app.js
@@ -1,22 +1,20 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Outlet} from "react-router-dom";
 import Header from "./moudel/header";
 import {Divider} from "antd";
 
-class App extends Component {
-    render() {
-        return (
-            <div className="app">
-                <section>
-                    <Header/>
-                </section>
-                <Divider/>
-                <section>
-                    <Outlet/>
-                </section>
-            </div>
-        );
-    }
+const App = () => {
+    return (
+        <div className="app">
+            <section>
+                <Header/>
+            </section>
+            <Divider/>
+            <section>
+                <Outlet/>
+            </section>
+        </div>
+    );
 }
 
 
--- src/pages/app/moudel/header.js
+++ src/pages/app/moudel/header.js
@@ -1,15 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {NavLink} from "react-router-dom";
-class Header extends Component {
-    render() {
-        return (
-            <div>
-                <NavLink to='/' style={ (isActive) => ({color: isActive ? 'red' : '#28cd00'}) }>首页</NavLink>
-                <NavLink to='/sonA' style={ (isActive) => ({color: isActive ? 'red' : '#28cd00'}) }>A组件</NavLink>
-                <NavLink to='/demo' style={ (isActive) => ({color: isActive ? 'red' : '#28cd00'}) }>demo</NavLink>
-            </div>
-        );
-    }
+
+const Header = () => {
+    return (
+        <div>
+            <NavLink to='/' style={(isActive) => ({color: isActive ? 'red' : '#28cd00'})}>首页</NavLink>
+            <NavLink to='/sonA' style={(isActive) => ({color: isActive ? 'red' : '#28cd00'})}>A组件</NavLink>
+            <NavLink to='/demo' style={(isActive) => ({color: isActive ? 'red' : '#28cd00'})}>demo</NavLink>
+        </div>
+    );
 }
 
 export default Header;
