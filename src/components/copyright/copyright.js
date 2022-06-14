df47f98a859858304d1aa0ffd62cd25f3be90657
zy <1840354092@qq.com>
2022-06-14 16:06:09 +0800

    copyright

--- src/components/copyright/copyright.js
+++ src/components/copyright/copyright.js
@@ -6,16 +6,17 @@ const Copyright = () => {
     return (
         <section className='copyright'>
             <ul>
-                <li>Copyright ©2022 </li>
-                <li>by<a href="https://gitee.com/Z568_568"> @书中枫叶 </a>.</li>
-                <li> All rights reserved. | <a
-                    href="https://beian.miit.gov.cn/#/Integrated/index">黔ICP备2022001745号-1</a></li>
-                <li>本站已运行：{runTimeInterval}</li>
+                <li style={{listStyleType:'none'}}><h4>资源链接</h4></li>
+                <li><a target='_blank' href="https://www.lodashjs.com/">Lodashjs</a></li>
+                <li><a target='_blank' href="https://zh-hans.reactjs.org/">React.v18 </a></li>
+                <li><a target='_blank' href="https://ant.design/index-cn">Ant.design</a></li>
+                <li><a target='_blank' href="https://webpack.js.org/">Webpack</a></li>
             </ul>
             <ul>
+                <li style={{listStyleType:'none'}}><h4>系统</h4></li>
                 <li>Copyright ©2022 </li>
-                <li>by<a href="https://gitee.com/Z568_568"> @书中枫叶 </a>.</li>
-                <li> All rights reserved. | <a
+                <li>by<a target='_blank' href="https://gitee.com/Z568_568"> @书中枫叶 </a>.</li>
+                <li> All rights reserved. | <a target='_blank'
                     href="https://beian.miit.gov.cn/#/Integrated/index">黔ICP备2022001745号-1</a></li>
                 <li>本站已运行：{runTimeInterval}</li>
             </ul>
--- src/index.js
+++ src/index.js
@@ -5,14 +5,11 @@ import './styles/index.scss';
 import Main from "./main/index";
 import {Provider} from "react-redux";
 import store from './store/index'
-import {Divider} from "antd";
-import Copyright from "./components/copyright/copyright";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
             <Main/>
-
         </Provider>,
     </React.StrictMode>
 );
