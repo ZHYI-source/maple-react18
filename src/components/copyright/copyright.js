947c9e4ad07d8bb3218d81d1a8d6cbb87b9a8a4c
zy <1840354092@qq.com>
2022-06-14 15:23:04 +0800

    richEdit

--- src/components/copyright/copyright.js
+++ src/components/copyright/copyright.js
@@ -12,6 +12,27 @@ const Copyright = () => {
                     href="https://beian.miit.gov.cn/#/Integrated/index">黔ICP备2022001745号-1</a></li>
                 <li>本站已运行：{runTimeInterval}</li>
             </ul>
+            <ul>
+                <li>Copyright ©2022 </li>
+                <li>by<a href="https://gitee.com/Z568_568"> @书中枫叶 </a>.</li>
+                <li> All rights reserved. | <a
+                    href="https://beian.miit.gov.cn/#/Integrated/index">黔ICP备2022001745号-1</a></li>
+                <li>本站已运行：{runTimeInterval}</li>
+            </ul>
+            <ul>
+                <li>Copyright ©2022 </li>
+                <li>by<a href="https://gitee.com/Z568_568"> @书中枫叶 </a>.</li>
+                <li> All rights reserved. | <a
+                    href="https://beian.miit.gov.cn/#/Integrated/index">黔ICP备2022001745号-1</a></li>
+                <li>本站已运行：{runTimeInterval}</li>
+            </ul>
+            <ul>
+                <li>Copyright ©2022 </li>
+                <li>by<a href="https://gitee.com/Z568_568"> @书中枫叶 </a>.</li>
+                <li> All rights reserved. | <a
+                    href="https://beian.miit.gov.cn/#/Integrated/index">黔ICP备2022001745号-1</a></li>
+                <li>本站已运行：{runTimeInterval}</li>
+            </ul>
         </section>
     );
 };
